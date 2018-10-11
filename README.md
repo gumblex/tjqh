705fbf71af10661b5d801819e5b8082c26a3d8f6
Dingyuan Wang <gumblex@aosc.io>
2018-10-11 12:25:11 +0800

    update 2010

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 国家统计局统计用区划代码
 
-爬取统计局数据，精确到乡镇街道。
+爬取[统计局数据](http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/)，精确到乡镇街道。已获取的数据在 data 目录。
 
-执行 `bash getdata.sh`。利用 wget 抓取网站，系统需要安装 tcl（一般都自带）。
+执行 `bash getdata.sh`。利用 wget 抓取网站，系统需要安装 iconv，tcl（一般都自带）。
 
--- getdata.sh
+++ getdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir data
 for year in $(seq 2009 2017); do
+if [ -f data/$year.csv ]; then continue; fi
 wget --mirror --page-requisites --adjust-extension --no-parent --convert-links --directory-prefix=statgov_tjqh http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/$year/index.html
 find statgov_tjqh/www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/$year -name '*.html' -exec env LC_ALL=C grep -a "<tr class='\(city\|county\|town\|village\)tr'>" {} \; | iconv -f gb18030 | tclsh tocsv.tcl > data/$year.csv
 done
