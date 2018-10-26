53386c4198f20c50e4e91a3f70e710935ed4c03f
Dingyuan Wang <gumblex@aosc.io>
2018-10-27 00:08:43 +0800

    fix wget arguments

--- getdata.sh
+++ getdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir data
 for year in $(seq 2009 2017); do
-if [ -f data/$year.csv ]; then continue; fi
-wget --mirror --page-requisites --adjust-extension --no-parent --convert-links --directory-prefix=statgov_tjqh http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/$year/index.html
-find statgov_tjqh/www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/$year -name '*.html' -exec env LC_ALL=C grep -a "<tr class='\(city\|county\|town\|village\)tr'>" {} \; | iconv -f gb18030 | tclsh tocsv.tcl > data/$year.csv
+if [ -f data/$year.csv -a ! -s data/$year.csv ]; then continue; fi
+wget -e robots=off --mirror --page-requisites --no-parent --convert-links --tries=10 --retry-on-http-error=400,429,500,504 --compression=auto --directory-prefix=statgov_tjqh http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/$year/index.html
+find statgov_tjqh/www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/$year -name '*.html' -exec env LC_ALL=C sed 's,</tr>,\0\n,g' {} \; | env LC_ALL=C grep -a "<tr class='\(city\|county\|town\|village\)tr'>" | iconv -f gb18030 | tclsh tocsv.tcl > data/$year.csv
 done
