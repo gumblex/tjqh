a743e726b2d5fef6869c35bb3078f4196afe7010
Dingyuan Wang <gumblex@aosc.io>
2019-04-30 11:06:21 +0800

    add 2018

--- getdata.sh
+++ getdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir data
-for year in $(seq 2009 2017); do
-if [ -f data/$year.csv -a ! -s data/$year.csv ]; then continue; fi
+for year in $(seq 2009 2018); do
+if [ -s data/$year.csv ]; then continue; fi
 wget -e robots=off --mirror --page-requisites --no-parent --convert-links \
   --tries=10 --retry-on-http-error=400,429,500,504 --compression=auto \
   --directory-prefix=statgov_tjqh \
